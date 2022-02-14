09b3758e09fa1058dbb775d7bdb0fcea3fbd9836
wenreq <294491328@qq.com>
2022-02-14 17:05:54 +0800

    fix-entry-js

---
+++ index.js
@@ -0,0 +1,16 @@
+export {
+  debounce,
+  throttle
+}
+  from './function/index.js'
+
+export {
+  clone,
+  deepClone
+}
+  from './object/index.js'
+
+export {
+  unique
+}
+from './array/index.js'
