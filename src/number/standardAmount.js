70a1c2548b765cef0afbbd619a0223a984e08b37
wenreq <294491328@qq.com>
2022-08-06 20:52:48 +0800

    ✨feat(number):整数和小数的金额格式函数

---
+++ src/number/standardAmount.js
@@ -0,0 +1,16 @@
+/**
+ * 将数字（包含小数）逢三一断
+ * @date 2022-08-06
+ * @param {Number} value 要转换的值
+ * @returns {String} 转换后的值
+ * @example standardAmount(999999999.9991) => '999,999,999.999'
+ */
+ function standardAmount(value) {
+    if (!value) {
+      return value === 0 ? '0' : '--'
+    }
+    const intPartFormat = parseFloat(value).toLocaleString()
+    return intPartFormat
+}
+
+module.exports = standardAmount;
---
+++ src/number/standardIntegerAmount.js
@@ -0,0 +1,16 @@
+/**
+ * 将整数数字逢三一断
+ * @date 2022-08-06
+ * @param {Number} value 要转换的值
+ * @returns {String} 转换后的值
+ * @example standardIntegerAmount(99999999999) => '99,999,999,999'
+ */
+function standardIntegerAmount(value) {
+    if (!value) {
+        return value === 0 ? '0' : '--'
+    }
+    const intPartFormat = value.toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,')
+    return intPartFormat
+}
+
+module.exports = standardIntegerAmount;
