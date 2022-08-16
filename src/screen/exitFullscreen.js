0844496ece9c4c49abe80c1b9141b58464513269
wenreq <294491328@qq.com>
2022-08-16 21:45:07 +0800

    ✨feat:exitFullscree()退出全屏

---
+++ src/screen/exitFullscreen.js
@@ -0,0 +1,20 @@
+/**
+ * 退出全屏
+ * @date 2022-08-16
+ */
+function exitFullscreen() {
+    let elem = parent.document;
+    elem.webkitCancelFullScreen
+        ? elem.webkitCancelFullScreen()
+        : elem.mozCancelFullScreen
+        ? elem.mozCancelFullScreen()
+        : elem.cancelFullScreen
+        ? elem.cancelFullScreen()
+        : elem.msExitFullscreen
+        ? elem.msExitFullscreen()
+        : elem.exitFullscreen
+        ? elem.exitFullscreen()
+        : alert("切换失败,可尝试Esc退出");
+}
+
+module.exports = exitFullscreen;
