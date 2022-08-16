b3ea7ce42e05b906a8b6329e0fd9cc3eab3b2b30
wenreq <294491328@qq.com>
2022-08-16 21:45:31 +0800

    ✨feat:toFullScreen()全屏

---
+++ src/screen/toFullScreen.js
@@ -0,0 +1,18 @@
+/**
+ * 描述: 全屏
+ * @date 2022-08-16
+ */
+function toFullScreen() {
+    let elem = document.body;
+        elem.webkitRequestFullScreen
+        ? elem.webkitRequestFullScreen()
+        : elem.mozRequestFullScreen
+        ? elem.mozRequestFullScreen()
+        : elem.msRequestFullscreen
+        ? elem.msRequestFullscreen()
+        : elem.requestFullScreen
+        ? elem.requestFullScreen()
+        : alert("浏览器不支持全屏");
+}
+
+module.exports = toFullScreen;
