9f8069517fb61c462a47baca57c2d53f63e43652
wenreq <294491328@qq.com>
2022-08-16 21:44:37 +0800

    ✨feat:base64数据导出文件，文件下载

---
+++ src/support/downloadFile.js
@@ -0,0 +1,27 @@
+/**
+ * 描述: base64数据导出文件，文件下载
+ * @date 2022-08-16
+ * @param {String} filename 文件名
+ * @param {base64} data Base64 数据
+ */
+function downloadFile(filename, data) {
+    let downloadLink = document.createElement('a');
+    if (downloadLink) {
+        document.body.appendChild(downloadLink);
+        downloadLink.style.display = 'none';
+        downloadLink.download = filename;
+        downloadLink.href = data;
+        if (downloadLink.createEvent) {
+            let downloadEvt = document.createEvent('MouseEvents');
+			downloadEvt.initEvent('click', true, false);
+			downloadEvt.dispatchEvent(downloadEvt);
+        } else if (document.createEventObject) {
+			downloadLink.fireEvent('onclick');
+		} else if (typeof downloadLink.onclick == 'function') {
+			downloadLink.onclick();
+		}
+        document.body.removeChild(downloadLink);
+    }
+}
+
+module.exports = downloadFile;
