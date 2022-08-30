827eece1e78e96d3a8bcb860f6bf4fdbf03c5c18
wcj <bxymvp9966@163.com>
2022-08-30 17:29:27 +0800

    :sparkles:feat: deepMergeObject()合并对象

--- src/object/deepMergeObject.js
+++ src/object/deepMergeObject.js
@@ -3,7 +3,7 @@
  * date:2022-8-30
  * @param {Object} target 目标对象
  * @param {Object} source 源对象
- * @param {Boolean} isCover 在目标对象中含有源对象的键时，是否覆盖，true为覆盖，false为不覆盖。默认是false
+ * @param {Boolean} isCover 同层级情况下，在目标对象中含有源对象的键时，是否覆盖，true为覆盖，false为不覆盖。默认是false
  * @returns {Object}
  */
 function deepMergeObject(target, source, isCover = true) {
@@ -20,9 +20,11 @@ function deepMergeObject(target, source, isCover = true) {
         sourcePropValue = source[prop];
         if (sourcePropValue && typeof sourcePropValue === 'object') {
             target[prop] = deepMergeObject(target[prop], sourcePropValue, isCover);
-        } else if (target.hasOwnProperty(prop) && isCover) {
-            target[prop] = sourcePropValue;
-        } else {
+        }
+        // 如果包含且需要覆盖
+        // 如果包含了不需要覆盖
+        // 没包含就不谈覆盖不覆盖了
+        if ((target.hasOwnProperty(prop) && isCover) || !target.hasOwnProperty(prop)) {
             target[prop] = sourcePropValue;
         }
     })
